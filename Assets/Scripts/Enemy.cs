7a773a5a21f31e00bca2265476694088c8057a2b
JoshuaE14 <estradajoshua761@gmail.com>
2025-03-31 09:39:49 -0700

    assets

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+//Enemy script, will store stats like health and behavior like movement
+
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+}
